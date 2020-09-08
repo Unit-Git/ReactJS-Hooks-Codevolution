07c8f04cc7a27756682d7ac39ad1e891bbf8bce6
Unit-Git <theunit954@gmail.com>
2020-09-09 02:12:22 +0700

    adding bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React, { useEffect } from "react";
-import { RootApp } from "./Styled-Components/HookStyled.js";
+import React, { Fragment } from "react";
 import "./scss/hook.scss";
-import ReducerApi from "./Components/useReducer/Reducer.api.jsx";
 import MainProjectHook from "./projectHook/MainProjectHook.jsx";
-import ReducerApi2 from "./Components/useReducer/Reducer.api2.jsx";
-// import DataFetching from "./Components/DataFetching.jsx";
-// import UseContextService from "./Components/UseContext/UseContext.service.jsx";
-// import CounterOneReducer from "./Components/useReducer/CounterOneReducer.jsx";
-// import CounterTwoReducer from "./Components/useReducer/CounterTwoReducer.jsx";
-// import UseCRService from "./Components/useContext-and-useReducer/UseCR.service.jsx";
 
+//* Styles
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import _MainHook from "./Components/_MainHook";
+
+// component
 function App() {
-  useEffect(() => {
-    document.title = "Hook Tutorial";
-  });
-
   return (
-    <RootApp>
-      {/* <ReducerApi /> */}
-      <MainProjectHook />
-      <ReducerApi2 />
-    </RootApp>
+    <Router>
+      <Fragment>
+        <Route path="/" exact component={_MainHook} />
+        <Route path="/shoe-shop" component={MainProjectHook} />
+      </Fragment>
+    </Router>
   );
 }
 
